2e259ecd841305423803df40c8366dd84660730a
gurumojo <theguy@gurumojo.net>
2022-10-22 18:00:28 -0600

    citation tweaks and image index

--- README.md
+++ README.md
@@ -38,11 +38,17 @@ npm start
 ```
 
 
+demonstration
+-------------
+
+Check out the [live deploy](https://presentation-archive.herokuapp.com/) of the
+most recent push to this respoitory.
+
+
 contact
 -------
 
 Tweet or DM [@thegurumojo](https://twitter.com/thegurumojo 'Neurodivergent Advocacy on Twitter')
 
 Support this work on [![Ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/gurumojo 'Buy me a coffee?')
-
 
--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
-const path = require('path')
+const { join } = require('path')
+const { readdir } = require('fs')
+
 const PORT = process.env.PORT || 8000
 
+const files = []
+
+readdir(join(__dirname, 'image'), { withFileTypes: true }, (error, array) => {
+	files.push(...array)
+})
+
+
 express()
-	.use(express.static(path.join(__dirname, 'archive')))
-	.use('/image', express.static(path.join(__dirname, 'image')))
-	.use('/dist', express.static(path.join(__dirname, 'node_modules/reveal.js/dist')))
-	.use('/plugin', express.static(path.join(__dirname, 'node_modules/reveal.js/plugin')))
-	.set('views', path.join(__dirname, 'view'))
+	.use(express.static(join(__dirname, 'archive')))
+	.use('/image', express.static(join(__dirname, 'image')))
+	.use('/dist', express.static(join(__dirname, 'node_modules/reveal.js/dist')))
+	.use('/plugin', express.static(join(__dirname, 'node_modules/reveal.js/plugin')))
+	.set('etag', false)
+	.set('powered-by', false)
+	.set('views', join(__dirname, 'view'))
 	.set('view engine', 'ejs')
-	.get('/', (req, res) => res.render('index'))
-	.get('/cool', (req, res) => res.send(cool()))
+	.get('/cool', (request, response) => response.send(cool()))
+	.get('*', (request, response) => response.render('index', { files }))
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
