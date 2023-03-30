7dde4c06d101384d759906dda2e85068b375a87e
gurumojo <theguy@gurumojo.net>
2023-03-30 16:13:57 -0600

    add group research charts

---
+++ archive/research-coding.js
@@ -0,0 +1,92 @@
+codes = [
+	{
+		name: 'desert',
+		sample: [
+			'Moab Utah',
+			'Red Rock',
+			'High Desert',
+			'in a desert',
+			'wilderness in Utah',
+			'near a canyon',
+		]
+	},
+	{
+		name: 'indoors',
+		sample: [
+			'indoors in the middle of the night while it was freezing',
+			'Dorm building in college',
+			'in a guides space set up for journeys',
+			'A space designed for shamanic journeying',
+		]
+	},
+	{
+		name: 'water',
+		sample: [
+			'along a river',
+			'on a beach under the stars',
+			'rocks and river nearby',
+		]
+	},
+	{
+		name: 'inexperienced',
+		sample: [
+			'n/a',
+			'Na',
+			'N/A',
+		]
+	},
+	{
+		name: 'shared',
+		sample: [
+			'Large group',
+			'social setting',
+			'Group Ceremony',
+		]
+	},
+	{
+		name: 'natural',
+		sample: [
+			'Red Rock',
+			'along a river',
+			'High desert',
+			'Outside in a desert',
+			'on a beach under the stars',
+			'by a fire',
+			'wilderness in Utah',
+			'huge stands of spruce trees',
+			'Warm, subtle natural lighting',
+		]
+	},
+	{
+		name: 'guided',
+		sample: [
+			'Group Ceremony',
+			'merged with a tree for a while',
+			'I was in a guides space set up for journeys',
+			'A space designed for shamanic journeying',
+		]
+	},
+	{
+		name: 'musical',
+		sample: [
+			'music library',
+			'instruments and quiet space',
+			'musical instruments and healing tools of the trade',
+		]
+	},
+	{
+		name: 'isolated',
+		sample: [
+			'Alone',
+			'I walked',
+			'I was in a guides space',
+		]
+	},
+	{
+		name: 'time',
+		sample: [
+			'middle of the night',
+			'under the stars, by a fire',
+		]
+	},
+]
